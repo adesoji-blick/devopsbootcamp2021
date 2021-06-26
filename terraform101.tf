0388b0d0213dc53f209a25d649dabcd3ea8bd466
adesoji <adesoji@blick.ng>
2021-06-26 12:13:34 -0600

    created variables & environment files

--- terraform101.tf
+++ terraform101.tf
@@ -15,14 +15,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "web_server" {
-  ami                    = "ami-07625b74039b1a58b"
-  instance_type          = "t2.micro"
-  key_name               = "devOps-task"
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.ssh_key
   user_data              = data.template_file.ec2_user_data.template
   vpc_security_group_ids = [aws_security_group.web_server_sg.id]
 
   tags = {
-    Name = "Web_Server"
+    Name = var.tag_name
   }
 }
 
