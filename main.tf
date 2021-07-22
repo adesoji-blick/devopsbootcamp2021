70f6a8551a1e221e7bd201bb156c43ec947ca37d
adesoji <adesoji@blick.ng>
2021-07-22 17:29:40 -0600

    week3tasks

--- main.tf
+++ main.tf
@@ -8,14 +8,22 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
-module "ec2-module" {
+module "Betateam" {
   source         = "./modules"
-  instance_count = 2
+  instance_count = 1
   ami_id         = "ami-07625b74039b1a58b"
   key            = "devOps-task"
   instance_type  = "t2.micro"
-  name           = "ec2-by-module"
+  name           = "Beta machine"
   httpport       = "80"
- }
+}
 
-
+module "FITteam" {
+  source         = "./modules"
+  instance_count = 1
+  ami_id         = "ami-07625b74039b1a58b"
+  key            = "devOps-task"
+  instance_type  = "t2.micro"
+  name           = "FIT machine"
+  httpport       = "8080"
+}
