d12decac34637c7b9cde391a8075bf934fed6049
adesoji <adesoji@blick.ng>
2021-07-27 11:33:41 -0600

    week 3 project code base

--- backend.tf
+++ backend.tf
@@ -1,7 +1,12 @@
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+}
+
 terraform {
   backend "s3" {
     bucket = "ijoseda-devops"
-    key    = "Devops2021/DevopsProject/project2-statefile"
+    key    = "Devops2021/DevopsProject/week3project-statefile"
     region = "ca-central-1"
   }
 }
