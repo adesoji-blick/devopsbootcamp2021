7bc11b10335cf0daa2c33a8091e81c0de1d78e79
adesoji <adesoji@blick.ng>
2021-08-14 12:45:03 -0600

    Docker Task

--- data-template.tf
+++
@@ -1,3 +0,0 @@
-data "template_file" "ansible_installation" {
-  template = file("${path.module}/bootstrap.txt")
-}
--- output.tf
+++
@@ -1,5 +0,0 @@
-output "toolServer-ip" {
-  value = aws_instance.tool-server.*.public_ip
-}
-
-#"App Server", "Web Server", "Database Server"
